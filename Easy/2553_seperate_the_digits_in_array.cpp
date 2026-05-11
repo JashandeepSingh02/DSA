c721627e3e5ff6448b5ff3a887385a1c42452ff5
JashandeepSingh02 <jashandeepsidhu8786@gmail.com>
2026-05-11 21:34:52 +0530

    Add solution for 2553 separate the digits in array

---
+++ Easy/2553_seperate_the_digits_in_array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> answer;
+
+        for (int num : nums) {
+            string s = to_string(num);
+
+            for (char ch : s) {
+                answer.push_back(ch - '0');
+            }
+        }
+
+        return answer;
+    }
+};
