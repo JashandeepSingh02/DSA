8031938c6bf67a1a42633b436b5f484a2ef8c666
JashandeepSingh02 <jashandeepsidhu8786@gmail.com>
2026-05-12 20:39:29 +0530

    Add Hard folder solutions

---
+++ Hard/1665_Minimum_initial_energy_complete_tasks.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), [](vector<int>& a, vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+
+        int ans = 0;
+        int currentEnergy = 0;
+
+        for (auto& task : tasks) {
+            int actual = task[0];
+            int minimum = task[1];
+
+            if (currentEnergy < minimum) {
+                ans += minimum - currentEnergy;
+                currentEnergy = minimum;
+            }
+
+            currentEnergy -= actual;
+        }
+
+        return ans;
+    }
+};
